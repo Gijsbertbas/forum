c485840b66901ef4bfbc1f14f7d85ac169c1f1ca
Gijs <gbstraathof@gmail.com>
2017-10-04 18:01:14 +0000

    updates and debug = false

--- forumreborn/run.py
+++ forumreborn/run.py
@@ -1,3 +1,22 @@
+from forum.models import ForumMessageModel
+import re
+
+# FOR WORDCLOUD
+def postswordcloud(reload=False):
+
+    if reload:
+        output = open('posts.dat','a')
+        for post in ForumMessageModel.objects.all():
+            output.write(post.body[65:-36])
+        output.close()
+
+        text = open('posts.dat').read()
+        text = re.sub('<([^<>]+)>','',text)
+        text = re.sub('\n','',text)
+        with open('posts.dat','w') as output:
+            output.write(text)
+
+'''
 for prins in prinsennamen:
     for ps in prins['pseudoniemen']:
         try:
@@ -5,20 +24,4 @@ for prins in prinsennamen:
         except:
             print('not in')
 
-# FOR WORDCLOUD
-output = open('posts.dat','a')
-for post in ForumMessageModel.objects.all():
-    output.write(post.body[65:-36])
-output.close()
-
-text = open('posts.dat').read()
-
-words=['<br>','je','de','het','een','ik','je','naar','<a href=','</a>']
-
-for word in words:
-    text.replace(word,'')
-    
-with open('text.dat','w') as output:
-    output.write(text)
-
-
+'''
