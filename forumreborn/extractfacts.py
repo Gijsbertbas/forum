a2e3c0610f31d0cefabb70db685421854a72a12e
Gijs <gbstraathof@gmail.com>
2017-10-06 09:43:46 +0000

    restructuring data analysis

--- forumreborn/extractfacts.py
+++ forumreborn/extractfacts.py
@@ -1,26 +1,31 @@
 '''
-extracting some interesting facts and figures from the database and print to screen
+extracting some interesting facts and figures from the database
 
 run from within django shell with:
-exec(open("data_analysis.py").read())
+exec(open("extractfacts.py").read())
 
-(including some legacy lines of code)
+This script saves all items as a list of lists/dictionaries/dataframes in a pickle file.
+To be used for graphics later.
 '''
 from django.db.models import Count, Max #,Q
 from forum.models import ForumMessageModel
 import operator
+import pickle
+import math
 
-print('\n TOTALS:')
-baarden = ForumMessageModel.get_root_nodes().count()
-print('%i baarden (%i index paginas)' % (baarden, baarden/20))
-print('%i posts' % ForumMessageModel.objects.all().count())
+# TOTALS:
+totals = {}
+totals['baarden'] = ForumMessageModel.get_root_nodes().count()
+totals['indexs'] = math.ceil(totals['baarden']/20)
+totals['posts'] = ForumMessageModel.objects.all().count()
 
-print('\n TOP 10 POSTERS:')
+# TOP 10 POSTERS:
 postsperprins = ForumMessageModel.objects.values('author').annotate(count=Count('author')).order_by('-count')
 for item in postsperprins[:10]:
     print('%i posts door %s' % (item['count'], item['author']))
-print('\n .. en nu inclusief alle pseudoniemen...')
-prinsennamen = [];
+
+# .. en nu inclusief alle pseudoniemen...
+prinsennamen = []
 prinsennamen.append({'naam': 'Gast', 'pseudoniemen': ['F','Folkert','f','goof','F`','fdboer','folkert','Goof','Goov','gahast!','goov','F de sloperd','gast']})
 prinsennamen.append({'naam': 'Roobin','pseudoniemen': ['Roobin','R','r','roobin','ROOBIN','Rooobin','Rwisman','R`','Roobin`','rOOBIN','Robert','roobvin','RRoobin','Roob in','Roobi','Robin','Roobinm','Roobinq','Roobinn','Rooin','@rwisman','RObert','ROobin','Rioobin','Roobin.','Roobion','Roobn','Roobni','Roonin','\roobin','`Roobin','robert','roobi','rrrrrooooooooooooobin','rwi18736','rwisman@gmail.com']})
 prinsennamen.append({'naam': 'Boogie', 'pseudoniemen': ['h','Hennie','H','Boogie','Hendrik','hennie','de P.C.','De P.C.','Hennei','de Porno Commissaris','de Porno-Commissaris','den P.C.','HEnnie','Henny','De Porno Commissaris','boogie','hendrik','De Porno-Commissaris','Henie','Hennnie','Porno Commissaris','de PC','BOOGSIEMEISTER DE COMPUMEISTER','Boogiie','De Porno-Commissars','HEnnei','Hendriquez','Henduhrik','HenniI','Hennie.','Hennied','Hennies','Hennii','Mijnheer de Porno Commissaris','`h','de P.Commissaris','h?','h`']})
@@ -39,20 +44,15 @@ for prins in prinsennamen:
         if item['author'] in prins['pseudoniemen']:
             posts+=item['count']
     postspp[prins['naam']] = posts
-postspp_sorted = sorted(postspp.items(), key=operator.itemgetter(1), reverse=True)
-_=[print('%i posts door %s'%(i[1],i[0])) for i in postspp_sorted]
+postspp = sorted(postspp.items(), key=operator.itemgetter(1), reverse=True)
+_ = [print('%i posts door %s'%(i[1],i[0])) for i in postspp]
 
-print('\n WORDCOUNT:')
-keywords = ['tieten','kut','geil','gast','gasten','bier','pils','brak','PrinsPils','USR']
-for key in keywords:
-    print('%i posts met het woord \'%s\'' % (ForumMessageModel.objects.filter(body__icontains=key).count(), key)) # 'icontains' should be case insensitive thought I didn't see diff with 'contains'
-
-print('\n PER YEAR:')
+# PER YEAR:
 for year in range(2001,2015):
     print('%i posts in %i' % (ForumMessageModel.objects.filter(timestamp__year=year).count(), year))
 
 print('\n LONGEST DISCUSSION:')
-maxdepth = ForumMessageModel.objects.all().aggregate(Max('depth'))['depth__max']
+maxdepth = ForumMessageModel.objects.all().aggregate(Max('depth'))['depth__max'] # change to numchild
 print('%i posts in de langste discussie' % maxdepth)
 print('posted on %s' % ForumMessageModel.get_root(ForumMessageModel.objects.filter(depth=maxdepth)[0]).timestamp) #could be more than 1
 
@@ -74,22 +74,6 @@ for year in range(2001,2015):
 
 data=[316,1158,2201,2097,2937,6538,4152,3169,2669,1350,790,75,16,1]
 py=pd.Series(data=data,index=pd.PeriodIndex(start=2001,end=2014)
-
-CORRECT POSTS WITH LINK:
-regex for URLs
-seems to work: '(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?'
-alternative: 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-
-import re
-match=re.compile(r'(?<=</a>)(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?')
-withlink = ForumMessageModel.objects.filter(body__contains="<a href=")
-for message in withlink:
-    message.body = re.sub(match,'',message.body)
-    message.save()
-
-CHECK MISSING POSTS:
-! cancel pipeline !
-scrapy crawl checkmissingposts -t csv -o missing.csv --loglevel=INFO
 
 GRAPPIG:
 1032350845
