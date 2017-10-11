9cad73998a0bf2314b08e15cc39a5850f6525ed5
Gijs <gbstraathof@gmail.com>
2017-10-11 07:22:20 +0200

    added static

--- forumreborn/forumreborn/settings.py
+++ forumreborn/forumreborn/settings.py
@@ -125,6 +125,6 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'mainapp', 'static'),
+            os.path.join(BASE_DIR, 'forum', 'static'),
             ]
 
